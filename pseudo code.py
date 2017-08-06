76e45443c81f9b880be382a0dc22d123fba9190a
noamm19-meet <noamm19@meet.mit.edu>
2017-08-06 16:29:18 +0300

    Stopping here

---
+++ pseudo code.py
@@ -0,0 +1,13 @@
+# make maze
+make blocks into a list
+make empty blocks into a list
+#make food
+make food appear in epmty blocks list
+when food is taken +1 to score
+#movemnt
+up ,down left right
+if pos is in blocks list dont move
+when player goes to house print you collected this much food 
+#timer
+count down the time
+when time = 0 print game over
