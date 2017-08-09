0ed7910248ad08a15cff5663572860d7ccc37d83
noamm19-meet <noamm19@meet.mit.edu>
2017-08-09 09:49:15 +0300

    Stopping here

--- omer-moving-7.8.17.py
+++ omer-moving-7.8.17.py
@@ -2,10 +2,11 @@ import turtle
 
 pos_list = []
 stamp_list= []
-food_pos = []
-food_stamps= []
+car_pos_l = []
+car_stamps_l= []
 
-
+SIZE_X = 400
+SIZE_Y = 600
 
 UP_EDGE = 250
 DOWN_EDGE = -250
@@ -14,15 +15,15 @@ LEFT_EDGE = -400
 
 turtle.setup(SIZE_X, SIZE_Y)
 ##makes the border box for the game:
-turtle.pensize(10)
-turtle.penup()
-turtle.goto(RIGHT_EDGE, UP_EDGE)
-turtle.pendown()
-turtle.goto(RIGHT_EDGE, DOWN_EDGE)
-turtle.goto(LEFT_EDGE, DOWN_EDGE)
-turtle.goto(LEFT_EDGE-10, UP_EDGE-10)
-turtle.goto(RIGHT_EDGE-10, UP_EDGE-10)
-turtle.penup()
+##turtle.pensize(10)
+##turtle.penup()
+##turtle.goto(RIGHT_EDGE, UP_EDGE)
+##turtle.pendown()
+##turtle.goto(RIGHT_EDGE, DOWN_EDGE)
+##turtle.goto(LEFT_EDGE, DOWN_EDGE)
+##turtle.goto(LEFT_EDGE-10, UP_EDGE-10)
+##turtle.goto(RIGHT_EDGE-10, UP_EDGE-10)
+##turtle.penup()
 
 
 turtle.register_shape('car.gif')
@@ -33,12 +34,13 @@ car.penup()
 turtle.hideturtle()
 
 
+
 UP_ARROW = "Up"
 LEFT_ARROW = "Left"
 DOWN_ARROW = "Down"
 RIGHT_ARROW = "Right"
 
-TIME_STEP = 100
+TIME_STEP = 500
 
 SPACEBAR = "space"
 
@@ -59,33 +61,32 @@ move_comand = [UP, DOWN, RIGHT, LEFT]
 
 def up():
     global direction
-    #if direction!= DOWN:
     direction= UP
+    move_car()
     print("You pressed the UP key")
 
 def down():
     global direction
-    #if direction != UP:
-    direction= DOWN  
+    direction= DOWN
+    move_car()
     print("You pressed the DOWN key")
 
 def left():
     global direction
-    #if direction != RIGHT:
     direction= LEFT
-    
+    move_car()
     print("You pressed the LEFT key")
 
 def right():
     global direction
-    #if direction!=LEFT:
     direction= RIGHT
+    move_car()
     print("You pressed the RIGHT key")
 
 def stop_move():
     global direction
     direction = SPACBAR
-    for i in range(2):
+    for i in range(1):
         print("You pressed the SPACBAR key")
 
 turtle.onkeypress( up, UP_ARROW)
@@ -101,21 +102,16 @@ def move_car():
     x_pos = my_pos[0]
     y_pos = my_pos[1]
 
-##MOVE THE CAR BY STEPS
-
-    
+##MOVE THE CAR BY STEPS:
     if direction == RIGHT:
         car.goto( x_pos+ SQUARE_SIZE, y_pos)
         print("you moved right!")
-        
     elif direction==LEFT:
         car.goto(x_pos-SQUARE_SIZE, y_pos)
         print("you moved left!")
-        
     elif direction == UP:
         car.goto( x_pos , y_pos+SQUARE_SIZE)
         print("you moved up!")
-
     elif direction == DOWN:
         car.goto( x_pos , y_pos-SQUARE_SIZE)
         print("you moved down!")
@@ -123,38 +119,39 @@ def move_car():
         print()
         
 
-    my_pos = car.pos()
-    new_pos= my_pos
-    new_x_pos = new_pos[0]
-    new_y_pos = new_pos[1]
-    pos_list.append(my_pos)
+    newcar_pos = car.pos()
+    new_x_pos = newcar_pos[0]
+    new_y_pos = newcar_pos[1]
+    car_pos_l.append(newcar_pos)
     
-    new_stamp = car.stamp()
-    stamp_list.append(new_stamp)
+    new_car_stamp = car.stamp()
+    car_stamps_l.append(new_car_stamp)
+    old_car_stamp = car_stamps_l.pop(0)
+    car.clearstamp(old_car_stamp)
+    car_pos_l.pop(0)
    
-    
-    if new_x_pos >= RIGHT_EDGE:
-        for t in range(200):
-            print("you hit the RIGHT edge! GAME OVER!")
-        quit()
-
-    if new_x_pos <= LEFT_EDGE:
-        for t in range(200):
-            print("you hit the LEFT edge! GAME OVER!")
-        quit()
-
-    if new_y_pos >= UP_EDGE:
-        for t in range(200):
-            print("you hit the TOP edge! GAME OVER!")
-        quit()
-
-    if new_y_pos <= DOWN_EDGE:
-        for t in range(200):
-            print("you hit the DOWN edge! GAME OVER!")
-        quit()
-     
+##BORDER    
+##    if new_x_pos >= RIGHT_EDGE:
+##        for t in range(200):
+##            print("you hit the RIGHT edge! GAME OVER!")
+##        quit()
+##
+##    if new_x_pos <= LEFT_EDGE:
+##        for t in range(200):
+##            print("you hit the LEFT edge! GAME OVER!")
+##        quit()
+##
+##    if new_y_pos >= UP_EDGE:
+##        for t in range(200):
+##            print("you hit the TOP edge! GAME OVER!")
+##        quit()
+##
+##    if new_y_pos <= DOWN_EDGE:
+##        for t in range(200):
+##            print("you hit the DOWN edge! GAME OVER!")
+##        quit()
+##     
         
     turtle.ontimer(move_car, TIME_STEP)
 
-if direction in move_comand:
-    move_car()
+
