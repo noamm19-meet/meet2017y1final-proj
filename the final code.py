ec2e11ea033387765ab4b0a1d45da0490703c52d
noamm19-meet <noamm19@meet.mit.edu>
2017-08-09 10:05:19 +0300

    Stopping here

--- the final code.py
+++ the final code.py
@@ -62,7 +62,7 @@ border.penup()
 # random_maze ( how many blocks )
 wall_list = random_maze(150)
 
-<<<<<<< HEAD
+
 
 ###########the food and the score#############
 score=0
@@ -136,13 +136,7 @@ def make_food():
         
         
 make_food()
-<<<<<<< HEAD
 
-
-make_food()
-
-=======
->>>>>>> 7a1067e7feaf8a1a13cfce50114ae8b36fccd694
 ############################################
 #car movement
 import turtle
@@ -151,9 +145,9 @@ sizex = 800
 sizey = 500
 
 turtle.setup(sizex,sizey)
-=======
+
 # car movement
->>>>>>> babe218c197cca147deb0ce68d61865809e6461f
+
 
 turtle.penup()
 
@@ -312,5 +306,66 @@ def make_food():
 
 make_food()
 ############################################
+
+
+
+import turtle
+
+
+import time
+
+
+
+turtle.hideturtle()
+turtle.penup()
+turtle.pensize(5)
+turtle.goto(250, 250)
+turtle.pendown()
+turtle.goto(250, 280)
+turtle.goto(180, 280)
+turtle.goto(180, 250)
+turtle.goto(250, 250)
+turtle.penup() 
+turtle.goto(215, 255)
+
+
+timer = turtle.clone()
+b = turtle.clone()
+b.penup()
+b.showturtle()
+b.shape("square")
+
+b.color("white")
+b.goto(223,265)
+
+def countdown(t):    
+    while t >= 0:
+        b.stamp()
+        timer.write(t)
+        time.sleep(1)
+        t -= 1        
+    quit()
+
+countdown(10)
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
